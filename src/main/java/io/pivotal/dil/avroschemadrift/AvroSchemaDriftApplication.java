c127fb48807403081779d69d734f581841c0ece5
Michael Goddard <mgoddard@gopivotal.com>
2017-12-05 15:34:21 -0500

    Initial version

---
+++ src/main/java/io/pivotal/dil/avroschemadrift/AvroSchemaDriftApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.dil.avroschemadrift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvroSchemaDriftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvroSchemaDriftApplication.class, args);
+	}
+}
