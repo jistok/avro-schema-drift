678e428ebf00ce9ed7a7e4637a160e262c610851
Michael Goddard <mgoddard@gopivotal.com>
2017-12-12 15:06:52 -0500

    Adding Redis bootstrap script

--- bootstrap_redis.sh
+++ bootstrap_redis.sh
@@ -2,17 +2,26 @@
 
 redis_log=$PWD/redis.log
 avro_schema="crimes_v1.avsc"
+redis_cli="./redis-stable/src/redis-cli"
 
-wget http://download.redis.io/redis-stable.tar.gz
-tar xvzf redis-stable.tar.gz
+echo "Downloading and building Redis"
+curl http://download.redis.io/redis-stable.tar.gz | tar xzvf -
 cd redis-stable
 make
 
+echo "Starting Redis server"
 nohup ./src/redis-server >> $redis_log 2>&1 </dev/null &
 echo "Redis PID: $!"
+sleep 1
+cd -
 
-# Enter the initial value, v1, of the schema into Redis
+read -p "Press enter to continue"
+
+echo "Setting the initial value, v1, of the column names in Redis"
 table_name=$( perl -ne 'print "$1\n" if /^\s+"namespace": "([^"]+)",\s*$/;' < $avro_schema )
 column_list=$( perl -ne 'print "$1\n" if /^\s+"doc": "([^"]+)",\s*$/;' < $avro_schema )
-echo "SET $table_name \"$column_list\"" | ./redis-stable/src/redis-cli
+echo "SET $table_name \"$column_list\"" | $redis_cli
+echo "GET $table_name" | $redis_cli
+
+echo "Done"
 
